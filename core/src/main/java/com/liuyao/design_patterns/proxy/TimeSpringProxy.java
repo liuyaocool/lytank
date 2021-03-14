703fcc5a20da211a0dfbefb961f2351e732d308f
刘耀 <zl_liuy@163.com>
2021-03-14 13:16:44 +0800

    aspectj-autoproxy

--- core/src/main/java/com/liuyao/design_patterns/proxy/TimeSpringProxy.java
+++ core/src/main/java/com/liuyao/design_patterns/proxy/TimeSpringProxy.java
@@ -1,11 +1,21 @@
 package com.liuyao.design_patterns.proxy;
 
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+/**
+ * 加配置： <aop:aspectj-autoproxy />
+ */
+@Aspect
 public class TimeSpringProxy {
 
+    @Before("execution(void com.liuyao.design_patterns.proxy.Tanko.move())")
     public void before() {
         System.out.println("TimeSpringProxy befoer execute " + System.currentTimeMillis());
     }
 
+    @After("execution(void com.liuyao.design_patterns.proxy.Tanko.move())")
     public void after() {
         System.out.println("TimeSpringProxy after execute " + System.currentTimeMillis());
     }
